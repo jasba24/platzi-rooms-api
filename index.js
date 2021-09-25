5e1a8b9ea57e1c8d6af4485db584b24ee4320b8a
jasba24 <johanbuitrago342@gmail.com>
2021-09-25 15:16:35 -0500

    add heroku config

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const routes = require("./routes")
 
 const init = async () => {
 	const server = Hapi.server({
-		port: 3000,
+		port: process.env.PORT || 3000,
 		host: "localhost",
 		routes: {
 			cors: true
--- mongo.js
+++ mongo.js
@@ -5,7 +5,12 @@ const { MONGO_DB_URI } = process.env
 const connectionString = MONGO_DB_URI
 
 mongoose
-	.connect(connectionString)
+	.connect(connectionString, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		useFindAndModify: true,
+		useCreateIndex: true,
+	})
 	.then(() => {
 		console.log("DB connected")
 	})
