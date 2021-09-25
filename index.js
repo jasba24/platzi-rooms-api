893602239135d2753b16c4745ef0e74c1a732525
jasba24 <johanbuitrago342@gmail.com>
2021-09-25 14:41:30 -0500

    add home page

--- index.js
+++ index.js
@@ -11,6 +11,27 @@ const init = async () => {
 	})
 
 	await server.register(routes)
+	server.route({
+		method: "GET",
+		path: "/",
+		handler: async (req, h) => {
+			return `<div>
+			<h1>Hello world</h1>
+			<h1>Routes</h1>
+			<ul>
+				<li>
+					<a href="api/rooms">api/rooms</a>
+				</li>
+				<li>
+					<a href="api/users">api/users</a>
+				</li>
+				<li>
+					<a href="api/services">api/services</a>
+				</li>
+			</ul>
+			</div>`
+		},
+	})
 	await server.start()
 	console.log(`Server running on ${server.info.uri}`)
 
